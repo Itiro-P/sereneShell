9ac7286ff57c2e59c3873b17283e03b066e8b0ba
Pedro N <96268046+Itiro-P@users.noreply.github.com>
2025-08-28 13:40:34 -0300

    cheatsheet

--- .config/ags/app.tsx
+++ .config/ags/app.tsx
@@ -6,6 +6,7 @@ import CavaOverlay from "./widget/CavaOverlay";
 import DTC from "./widget/DTC";
 import { Gtk } from "ags/gtk4";
 import GObject from "gi://GObject?version=2.0";
+import Cheatsheet from "./widget/Cheatsheet";
 
 function main() {
     const monitors = createBinding(app, "monitors");
@@ -16,6 +17,7 @@ function main() {
                     <Bar gdkmonitor={monitor} />
                     <CavaOverlay gdkmonitor={monitor} />
                     <DTC gdkmonitor={monitor} />
+                    <Cheatsheet gdkmonitor={monitor} />
                 </This>
             )}
         </For>
--- .config/ags/widget/Cheatsheet.tsx
+++ .config/ags/widget/Cheatsheet.tsx
@@ -1,22 +1,78 @@
 import { onCleanup } from "ags";
-import { Astal, Gdk } from "ags/gtk4"
+import { Astal, Gdk, Gtk } from "ags/gtk4"
 import app from "ags/gtk4/app";
 
-// WIP
+function Entry({ bind, action }: { bind: string, action: string }) {
+    return (
+        <box cssClasses={['Entry']} overflow={Gtk.Overflow.HIDDEN} homogeneous>
+            <label cssClasses={["Bind"]} label={bind} />
+            <label cssClasses={["Action"]} label={action} />
+        </box>
+    );
+}
 
-export default function CavaOverlay({ gdkmonitor }: { gdkmonitor: Gdk.Monitor }) {
-    const { LEFT, RIGHT, BOTTOM, TOP } = Astal.WindowAnchor;
+export default function Cheatsheet({ gdkmonitor }: { gdkmonitor: Gdk.Monitor }) {
 
     return (
         <window
-            namespace='Cheatsheet'
+            name='Cheatsheet'
             layer={Astal.Layer.OVERLAY}
-            exclusivity={Astal.Exclusivity.IGNORE}
             gdkmonitor={gdkmonitor}
-            anchor={RIGHT | LEFT | BOTTOM | TOP}
             application={app}
             $={(self) => onCleanup(() => self.destroy())}
         >
+            <box cssClasses={['Cheatsheet']} orientation={Gtk.Orientation.VERTICAL}>
+                <label cssClasses={['Title']} label={'Cheatsheet'} />
+                <box cssClasses={['Half']} homogeneous>
+                    <box cssClasses={['Section']} orientation={Gtk.Orientation.VERTICAL}>
+                        <label cssClasses={["Subtitle"]} label={"Gestão de Clientes"} />
+                        <box orientation={Gtk.Orientation.VERTICAL}>
+                            <Entry bind="SUPER + P" action="Fixar cliente" />
+                            <Entry bind="SUPER + Q" action="Fechar programa" />
+                            <Entry bind="SUPER + V" action="Flutuar a tela" />
+                            <Entry bind="SUPER + F" action="Forçar tela cheia" />
+                            <Entry bind="SUPER + J" action="Dividir em Dwindle" />
+                            <Entry bind="SUPER + " action="Mover foco para janela acima" />
+                            <Entry bind="SUPER + " action="Mover foco para janela abaixo" />
+                            <Entry bind="SUPER + " action="Mover foco para janela à esquerda" />
+                            <Entry bind="SUPER + " action="Mover foco para janela à direita" />
+                        </box>
+                    </box>
+                    <box cssClasses={['Section']} orientation={Gtk.Orientation.VERTICAL}>
+                        <label cssClasses={["Subtitle"]} label={"Gestão de Workspaces"} />
+                        <box orientation={Gtk.Orientation.VERTICAL}>
+                            <Entry bind="CTRL + SUPER + " action="Ir para Workspace à esquerda" />
+                            <Entry bind="CTRL + SUPER + " action="Ir para Workspace à direita" />
+                            <Entry bind="CTRL + SHIFT + SUPER + " action="Mover para Workspace à esquerda" />
+                            <Entry bind="CTRL + SHIFT + SUPER + " action="Mover para Workspace à direita" />
+                            <Entry bind="SUPER + S" action="Ir para Workspace especial" />
+                            <Entry bind="SUPER + SHIFT + S" action="Mover para workspace especial" />
+                        </box>
+                    </box>
+                </box>
+                <box cssClasses={['Half']} homogeneous>
+                    <box cssClasses={['Section']} orientation={Gtk.Orientation.VERTICAL}>
+                        <label cssClasses={["Subtitle"]} label={"Programas padrão"} />
+                        <box orientation={Gtk.Orientation.VERTICAL}>
+                            <Entry bind="SUPER + Enter" action="Terminal" />
+                            <Entry bind="SUPER + R" action="Lançador de aplicativos" />
+                            <Entry bind="SUPER + E" action="Abrir Gerenciador de Arquivos" />
+                            <Entry bind="SUPER + D" action="Abrir/Fechar Dock" />
+                            <Entry bind="SUPER + C" action="Abrir/Fechar Cheatsheet" />
+                            <Entry bind="SUPER + ESC" action="Abrir/fechar Wlogout" />
+                        </box>
+                    </box>
+                    <box cssClasses={['Section']} orientation={Gtk.Orientation.VERTICAL}>
+                        <label cssClasses={["Subtitle"]} label={"Outros"} />
+                        <box orientation={Gtk.Orientation.VERTICAL}>
+                            <Entry bind="PRINT" action="Capturar tela" />
+                            <Entry bind="SUPER + PRINT" action="Capturar região" />
+                            <Entry bind="SUPER + M" action="Fechar sessão" />
+                            <Entry bind="SUPER + L" action="Bloquear a tela" />
+                        </box>
+                    </box>
+                </box>
+            </box>
         </window>
     );
 }
